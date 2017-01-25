879b126efade8a9309343cc42297843de257fcd3
Justin S <justinsr20@gmail.com>
2017-01-25 16:14:52 +1000

    stable script to start up jenkins after reboot

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -2,8 +2,15 @@
 
 systemctl start docker
 
-# remove old exited containers
+# remove old stuff
 docker rm -v $(docker ps -a -q -f status=exited)
+docker rmi $(docker images -f "dangling=true" -q)
+
+# removes everything (warning including docker volumes)
+#docker system prune -f
 
 # initial start of jenkins container
 docker run -d -p 8080:8080 -v jenkins_home:/var/jenkins_home --name=jenkins-master judd/jenkins
+
+# remove volumes not being used
+docker volume rm $(docker volume ls -qf dangling=true)
